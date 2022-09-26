636c96086a3416be5679c96977f4e30def904a09
UsielOM <usiel31197@outlook.es>
2022-09-26 12:23:38 -0500

    Implementando estilos de slots

---
+++ src/ejemplo-cinco.js
@@ -0,0 +1,21 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class EjemploCinco extends LitElement {
+
+    static get styles() {
+        return css `
+        ::slotted(*) { font-family: Roboto;}
+        ::slotted(p) {color: blue;}
+        div ::slotted(*) {color: red;}
+
+        `;
+    }
+
+    render() {
+        return html `
+        <slot></slot>
+        <div><slot name="hi"></slot></div>
+        `;
+    }
+}
+customElements.define('ejemplo-cinco', EjemploCinco);
